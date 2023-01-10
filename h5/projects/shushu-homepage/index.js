acc310566ac1c6227cbdf94ded9237ae68d5ee16
SimonW <39937682+wxm546@users.noreply.github.com>
2023-01-11 00:19:45 +0800

    Site updated: 2023-01-11 00:19:43

---
+++ h5/projects/shushu-homepage/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+	anmimationPreview();
+	var pjax = new Pjax({
+		elements: "a", // default is "a[href], form[action]"
+		selectors: ["main"],
+		cacheBust: false, //When set to true, Pjax appends a timestamp query string segment to the requested URL in order to skip the browser cache.
+		scrollRestoration: false, //When set to true, Pjax will attempt to restore the scroll position when navigating backwards or forwards.
+		switches: {
+			main: Pjax.switches.sideBySide,
+		},
+		switchesOptions: {
+			main: {
+				//动画效果
+				classNames: {
+					remove: "Animated-easeOut Animate--noDelay",
+					add: "Animated-easeIn",
+					backward: "Animated-easeOut",
+					forward: "Animated-easeOut",
+				},
+			},
+		},
+	});
+});
+
+document.addEventListener("pjax:complete", function () {
+	//pjax时重载点js脚本 pjax_reload()
+	anmimationPreview();
+});
+
+function anmimationPreview() {
+	$(".previewbutton").click(function () {
+		var buttonId = $(this).attr("id"); //modal-one
+		$("#modal-container").removeAttr("class").addClass(buttonId);
+		//点击添加图片
+		let imgsrc = $(this).children("img").attr("src");
+		imgsrc = imgsrc.replace("min50/", "");
+		let imgalt = $(this).children("img").attr("alt");
+		// $(".modal").append("<h2>I'm a Modal</h2><p>Hear me roar.</p>")
+		//DONE:通过直接给图片添加属性解决大小问题
+		$(".modal-img").html(`<img class="gallary-img-view" src="${imgsrc}" alt="${imgalt}" style="width:auto; height: 100vh; max-width:100%; " />`);
+		$("body").addClass("modal-active");
+	});
+
+	$("#modal-container").click(function () {
+		$(this).addClass("out");
+
+		$("body").removeClass("modal-active");
+		//离开时候清空modal
+		// $(".modal").empty();
+	});
+}
