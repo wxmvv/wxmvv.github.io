b8c6954cf781c067150237c5a382e0d39baf3887
wxm <39937682+wxm546@users.noreply.github.com>
2023-02-09 10:35:46 +0800

    Site updated: 2023-02-09 10:35:45

--- js/main.js
+++ js/main.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
   /**
    * Shows the responsive navigation menu on mobile.
    */
-  // $("#header > #nav > ul > .icon").click(function() {
-  //   $("#header > #nav > ul").toggleClass("responsive");
-  // });
+  $("#header > #nav > ul > .icon").click(function() {
+    $("#header > #nav > ul").toggleClass("responsive");
+  });
 
 
   /**
